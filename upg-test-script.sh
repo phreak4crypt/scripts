deb4a4c306335c31bd34cd4978a4d1175dbc2343
crypt0ph <47079754+crypt0ph@users.noreply.github.com>
2020-03-01 23:28:19 +0530

    Create upg-test-script.sh
    test script for UpGrad

---
+++ upg-test-script.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+pcount=0
+fcount=0
+for d in /code/testcases/*; do
+        cp $d/eval.py3 test.py
+        python test.py &>> $d/OUTPUT
+        if [ $? -ne 0 ]; then
+                fcount=$((fcount+1))
+        else
+                pcount=$((pcount+1))
+        fi
+        rm test.py
+done
+echo "Number of passed testcases is: "$pcount
+echo
+echo "Number of failed testcases is: "$fcount
