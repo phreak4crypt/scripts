ce4969631e3bb1a858b4dd1f5e13b2038ebe879c
crypt0ph <47079754+crypt0ph@users.noreply.github.com>
2020-09-28 06:32:10 +0530

    Update jsp-test-script.sh

--- jsp-test-script.sh
+++ jsp-test-script.sh
@@ -9,13 +9,13 @@ if [ -f eval.class ]; then
 fi
 
 for d in /code/testcases/*; do
-    mv $d/eval.java /home/user/workspace
+    cp $d/eval.java /home/user/workspace
     javac -cp .:/home/user/jars/* eval.java 
     java -cp .:/home/user/jars/* org.junit.runner.JUnitCore eval  &> temp
-    if [ $? -ne 0 ]; then
-    	fcount=$((fcount+1))
+    if [ $? -eq 0 ]; then
+    	pcount=$((pcount+1))
 	else
-	    pcount=$((pcount+1))
+	    fcount=$((fcount+1))
 	fi
     echo -n $exit_status > $d/STATUS
     rm eval.class
