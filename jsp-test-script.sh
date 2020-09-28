89737cbbd883d49df6b862fe44fd8e323a39b2e6
crypt0ph <47079754+crypt0ph@users.noreply.github.com>
2020-09-28 06:56:55 +0530

    Update jsp-test-script.sh

--- jsp-test-script.sh
+++ jsp-test-script.sh
@@ -11,12 +11,13 @@ fi
 for d in /code/testcases/*; do
     cp $d/eval.java /home/user/workspace
     javac -cp .:/home/user/jars/* eval.java 
-    java -cp .:/home/user/jars/* org.junit.runner.JUnitCore eval  &> temp
-    if [ $? -eq 0 ]; then
+    java -cp .:/home/user/jars/* org.junit.runner.JUnitCore eval
+    exit_status=$?
+    if [ $exit_status -eq 0 ]; then
     	pcount=$((pcount+1))
-	else
-	    fcount=$((fcount+1))
-	fi
+    else
+        fcount=$((fcount+1))
+    fi
     echo -n $exit_status > $d/STATUS
     rm eval.class
     rm temp
@@ -25,3 +26,4 @@ done
 echo "Number of passed testcases is: "$pcount
 echo
 echo "Number of failed testcases is: "$fcount
+
